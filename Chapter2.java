8f71ede4c2fac10f2fa8e60921914d37c212cf9a
zun clever <estro.zun@gmail.com>
2021-06-02 22:10:54 +0700

    first commit

---
+++ Chapter2.java
@@ -0,0 +1,19 @@
+/*
+
+  This program belong to excercise Chapter 2
+  
+*/
+public class Chapter2{
+    public static void main(String[] args) {
+        
+        int date,year;
+        String day,mounth;
+        day = "Wednesday";
+        mounth = "June";
+        date = 2;
+        year = 6;
+
+    
+    }
+
+}
