f2a4ad113e64faae313f7619e2b3697df9afb3d5
zun clever <estro.zun@gmail.com>
2021-06-02 22:40:13 +0700

    Complete at 4.

--- Time.java
+++ Time.java
@@ -1,10 +1,32 @@
 /*
-    This program Exercise 2.3
+    This program belong to  Exercise 2.3
 */
 public class Time {
     public static void main(String[] args) {
+        
         //Declare Variable
-        int 
+        int hour,minute,second;
+        int secondPerDay;
+        
+        //Assign value
+        hour = 22;
+        minute = 30;
+        
+        //Calculate second since midnight and Display
+        second = (22*60*60) + (30*60);
+        System.out.print("Second since midnight: ");
+        System.out.println(second);
+
+        //Calculate second remaining of the day and Display
+        secondPerDay = 24*60*60;
+        second = secondPerDay - second;
+        System.out.print("Second since midnight: ");
+        System.out.println(second);
+
+        //
+
+
+
     }
     
 }
