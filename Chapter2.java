4ad8f56f1f670f0da3d7a069e9b248bfc6589c17
zun clever <estro.zun@gmail.com>
2021-06-02 22:25:21 +0700

    Success

--- Chapter2.java
+++ Chapter2.java
@@ -5,13 +5,23 @@
 */
 public class Chapter2{
     public static void main(String[] args) {
-        
+      //Declare Variable
         int date,year;
         String day,mounth;
+      
+      //initialize variable
         day = "Wednesday";
         mounth = "June";
         date = 2;
-        year = 6;
+        year = 2021;
+      
+      //Display 
+      //System.out.printf("%s, %s %2d, %2d", day, mounth, date, year);
+
+      //Display in any format
+      System.out.printf("American format: %s, %s %02d, %2d\n", day, mounth, date, year);
+      System.out.printf("European format: %s %02d %s %2d", day, date, mounth, year);
+
 
     
     }
