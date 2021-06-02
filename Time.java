b4db92e2f2bfc3f9b07b15dac82582617d4fbfc7
zun clever <estro.zun@gmail.com>
2021-06-02 22:51:38 +0700

    Success

--- Time.java
+++ Time.java
@@ -5,25 +5,30 @@ public class Time {
     public static void main(String[] args) {
         
         //Declare Variable
-        int hour,minute,second;
+        int hour,minute,secondP,secondR;
         int secondPerDay;
+        double percent;
         
         //Assign value
         hour = 22;
         minute = 30;
         
         //Calculate second since midnight and Display
-        second = (22*60*60) + (30*60);
+        secondP = (22*60*60) + (30*60);
         System.out.print("Second since midnight: ");
-        System.out.println(second);
+        System.out.println(secondP);
 
         //Calculate second remaining of the day and Display
         secondPerDay = 24*60*60;
-        second = secondPerDay - second;
+        secondR = secondPerDay - secondP;
         System.out.print("Second since midnight: ");
-        System.out.println(second);
+        System.out.println(secondR);
 
-        //
+        //Percentage of the day has passed
+        percent = (double)secondP / (double)secondPerDay;
+        percent = percent * 100;
+        System.out.print("Percent: ");
+        System.out.println(percent);
 
 
 
