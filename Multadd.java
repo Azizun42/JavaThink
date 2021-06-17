2f4be77a0e4994b4b5d39dd3e2fe6df629e4ccc1
zun clever <estro.zun@gmail.com>
2021-06-17 21:20:58 +0700

    Add method exp and debug

--- Multadd.java
+++ Multadd.java
@@ -3,6 +3,11 @@ public class Multadd{
 
         return a*b+c;
     }
+    public static double exp(double x, double y) {
+        return 1.1;
+    }
+
+    
 
     public static void main(String[] args) {
         System.out.println(multadd(1.0, 2.0, 3.0));
