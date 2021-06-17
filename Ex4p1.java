2b8ce4b46c7df547c5aa5ac93f6d832262139583
zun clever <estro.zun@gmail.com>
2021-06-17 11:13:50 +0700

    Write a method for printing American Standard format time

--- Ex4p1.java
+++ Ex4p1.java
@@ -2,7 +2,8 @@ public class Ex4p1 {
 
     public static void printAmerican(String day, int date, String mounth, int year) {
         System.out.print(day);
-        System.out.print();
+        System.out.print(", ");
+        System.out.print(mounth);
         
     }
     
