1f330df49b60dfdd5821ed5ff2050d769c84fbed
zun clever <estro.zun@gmail.com>
2021-06-17 11:10:55 +0700

    This file belong to Exercsie 4.1

---
+++ Ex4p1.java
@@ -0,0 +1,9 @@
+public class Ex4p1 {
+
+    public static void printAmerican(String day, int date, String mounth, int year) {
+        System.out.print(day);
+        System.out.print();
+        
+    }
+    
+}
