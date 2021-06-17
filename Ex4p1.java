7cbf7cd7e42d30422e180f0537fe1dc0acf45487
zun clever <estro.zun@gmail.com>
2021-06-17 11:39:46 +0700

    Add function allow user to input they own date

--- Ex4p1.java
+++ Ex4p1.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ex4p1 {
 
     public static void printAmerican(String day, int date, String mounth, int year) {
@@ -16,8 +18,20 @@ public class Ex4p1 {
         int date, year;
         String day, mounth;
 
+        //Input information from user
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input your information [Day] : ");
+        day = in.nextLine();
+        System.out.print("Input your information [Date] : ");
+        date = in.nextInt();
+        in.nextLine();
+        System.out.print("Input your information [Mount] : ");
+        mounth = in.nextLine();
+        System.out.print("Input your information  [Year] : ");
+        year = in.nextInt();
+
         //Calling method for print out
-        printAmerican("Wednesday", 17, "June", 2021);
+        printAmerican(day, date, mounth, year);
 
     }
     
